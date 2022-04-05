c754779313f01350c822afa5f538fd78d4d0ff1b
bilalbakartepe <bilalbakartepe@aof.anadolu.edu.tr>
2022-04-05 22:34:29 +0300

    Second Commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta content="width=device-width, initial-scale=1.0" name="viewport">
+
+  <title>EstateAgency Bootstrap Template - Index</title>
+  <meta content="" name="description">
+  <meta content="" name="keywords">
+
+</head>
+
+<body>
+  
+</body>
+
+</html>
