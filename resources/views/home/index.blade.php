cf026de779610e6ac24b9c7a8b13f508dfed1bf5
bilalbakartepe <bilalbakartepe@aof.anadolu.edu.tr>
2022-04-10 17:48:42 +0300

    fourth commit

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -1,12 +1,42 @@
-@extends('layouts.home')
 
-  @section('tittle','It is index Page')
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>@yield('tittle')</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
 
-@section('sidebar')
-    @parent
-    <p> Master page sidebar</p>
-@endsection
+</head>
+<body>
 
-@section('content')
-<p>body content</p>
-@endsection
+<div class="jumbotron text-center">
+  <h1>HEADER</h1>
+  <p>Resize this responsive page to see the effect!</p> 
+</div>
+  
+<div class="container">
+  <div class="row">
+    <div class="col-sm-4">
+      <h3>Side Bar</h3>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit...</p>
+      <p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris...</p>
+    </div>
+    <div class="col-sm-8">
+        <div>
+            <h1>SLIDE</h1>
+            <br>
+            <br>
+            <hr>
+        </div>
+      <h3>Content</h3>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit...</p>
+      <p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris...</p>
+    </div>
+  </div>
+</div>
+<div class="jumbotron text-center">
+  <p>Footer</p>
+</div>
+</body>
+</html>
