4a869d490b52b0ba7b4dea4d1e030dce54a50064
bilalbakartepe <bilalbakartepe@aof.anadolu.edu.tr>
2022-04-11 17:28:48 +0300

    seventh commit

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\AdminPanel\HomeController as AdminHomeController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -29,3 +30,5 @@ Route::get('/contact',function(){
 Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
     return view('dashboard');
 })->name('dashboard');
+
+Route::get('/admin',[AdminHomeController::class,'index'])->name('admin');
