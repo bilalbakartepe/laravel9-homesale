2db43073a3c13821bfcc346139da7693cc84984b
bilalbakartepe <bilalbakartepe@aof.anadolu.edu.tr>
2022-04-12 02:28:18 +0300

    ninth commit

---
+++ database/migrations/2022_04_11_231257_create_comments_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('comments', function (Blueprint $table) {
+            $table->id();
+            $table->string('comment');
+            $table->integer('rate');
+            $table->foreignId('parentid');
+            $table->foreignId('user_id');
+            $table->string('ip');
+            $table->string('status');
+            $table->timestamp('expire_at');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('comments');
+    }
+};
