8baa286f2b48204ddeb601ffa040956434a3f51d
bilalbakartepe <bilalbakartepe@aof.anadolu.edu.tr>
2022-04-16 00:09:56 +0300

    twentynineth commit

--- app/Models/Home.php
+++ app/Models/Home.php
@@ -8,7 +8,7 @@ use Illuminate\Database\Eloquent\Model;
 class Home extends Model
 {
     use HasFactory;
-
+    public $timestamps = false;
     public function category(){
         return $this->belongsTo(Category::class);
     }
